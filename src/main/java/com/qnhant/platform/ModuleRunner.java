48ba1e34c4c911cd78274dd7fb43e3c45e4f70c9
Quoc Nhat Han TRAN <quocnhathantran132@gmail.com>
2024-11-16 22:57:12 +0100

    Define common interface

---
+++ src/main/java/com/qnhant/platform/ModuleRunner.java
@@ -0,0 +1,10 @@
+package com.qnhant.platform;
+
+/**
+ * Interface to indicate what a module should behave
+ */
+public interface ModuleRunner {
+    String getIdentifier();
+
+    String getResponse();
+}
